750b2ff29d83845149ea489a5f10974f4e51d92f
Maximilian Bärtel <maximilian.baertel@mum.de>
2026-06-01 08:42:54 +0200

    Add ISimulationState

--- RulesResolver.Core/Simulation/ISimulationState.cs
+++ RulesResolver.Core/Simulation/ISimulationState.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using RulesResolver.Metadata.Ids;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace RulesResolver.Core.Simulation
 {
-    internal interface ISimulationState
+    public interface ISimulationState
     {
+        T Get<T>(StateKey key);
+        void Set<T>(StateKey key,T value);
+        bool Has<T>(StateKey key);
     }
 }
